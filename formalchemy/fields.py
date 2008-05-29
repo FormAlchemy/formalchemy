314579c56a734d0f8a5e83c76de53e1cda9f57ba
jbellis <devnull@localhost>
2008-05-29 16:27:35 +0000

    split common parts of AttributeRenderer into AbstractRenderer.  Add AdditionalRenderer (incomplete) for 'extra' fields in the form.

--- formalchemy/fields.py
+++ formalchemy/fields.py
@@ -185,30 +185,6 @@ def query_options(query):
     return [(str(item), _pk(item)) for item in query.all()]
 
 
-def unstr(attr, st, force_scalar=False):
-    """convert st (raw user data, or None) into the data type expected by attr"""
-    assert isinstance(attr, AttributeRenderer)
-    if attr.is_collection() and not force_scalar:
-        return [attr.query(attr.collection_type()).get(unstr(attr, id_st, True))
-                for id_st in st]
-    if isinstance(attr.type, types.Boolean):
-        return st is not None
-    if st is None:
-        return None
-    if isinstance(attr.type, types.Integer):
-        try:
-            return int(st)
-        except:
-            raise validators.ValidationException('Value is not an integer')
-    if isinstance(attr.type, types.DateTime):
-        # todo
-        pass
-    if isinstance(attr.type, types.Date):
-        # todo
-        pass
-    return st
-
-
 def _foreign_keys(property):
     # 0.4/0.5 compatibility fn
     try:
@@ -217,21 +193,14 @@ def _foreign_keys(property):
         return [r for l, r in property.synchronize_pairs]
 
 
-class AttributeRenderer(object):
+class AbstractRenderer(object):
     """
     Contains the information necessary to render (and modify the rendering of)
-    an SQLAlchemy attribute.
+    a form field
     """
-    def __init__(self, instrumented_attribute, parent):
+    def __init__(self, parent):
         # the FieldSet (or any ModelRenderer) owning this instance
         self.parent = parent
-        # we rip out just the parts we care about from InstrumentedAttribute.
-        # impl is the AttributeImpl.  So far all we care about there is ".key,"
-        # which is the name of the attribute in the mapped class.
-        self._impl = instrumented_attribute.impl
-        # property is the PropertyLoader which handles all the interesting stuff.
-        # mapper, columns, and foreign keys are all located there.
-        self._property = instrumented_attribute.property
         # what kind of Field to render this attribute as.  this will be autoguessed,
         # unless the user forces it with .dropdown, .checkbox, etc.
         self.render_as = None
@@ -245,9 +214,9 @@ class AttributeRenderer(object):
         self.modifier = None
         # label to use for the rendered field.  autoguessed if not specified by .label()
         self.label_text = None
-        # default "required" value -- may be overriden to True by .required()
-        self._required = (not self.is_collection() and not self._column.nullable)
-            
+        # default = not required; may be overriden to True by .required()
+        self._required = False
+
     def __deepcopy__(self, memo):
         wrapper = copy(self)
         wrapper.render_opts = dict(self.render_opts)
@@ -257,106 +226,21 @@ class AttributeRenderer(object):
                         
     def is_raw_foreign_key(self):
         """True iff this attribute is a raw foreign key"""
-        try:
-            return _foreign_keys(self._property.columns[0])
-        except AttributeError:
-            return False
-        
+        return False
+
     def is_pk(self):
         """True iff this attribute is a primary key"""
-        return self._column.primary_key
-    
-    def type(self):
-        """The SQLAlchemy type of this attribute"""
-        return self._column.type
-    type = property(type)
+        return False
 
-    def _column(self):
-        # todo this does not allow handling composite attributes (PKs or FKs)
-        if isinstance(self._impl, ScalarObjectAttributeImpl):
-            # If the attribute is a foreign key, return the Column that this
-            # attribute is mapped from -- e.g., .user -> .user_id. 
-            return _foreign_keys(self._property)[0]
-        elif isinstance(self._impl, ScalarAttributeImpl):
-            # normal property, mapped to a single column from the main table
-            return self._property.columns[0]
-        else:
-            # collection -- use the mapped class's PK
-            assert isinstance(self._impl, CollectionAttributeImpl)
-            return self._property.mapper.primary_key[0]
-    _column = property(_column)
-    
-    def key(self):
-        """The name of the attribute in the class"""
-        return self._impl.key
-    key = property(key)
-
-    def name(self):
-        """ 
-        The name of the form input. usually the same as the column name, except for
-        multi-valued SA relation properties. For example, for order.user,
-        name will be user_id (assuming that is indeed the name of the foreign
-        key to users). 
-        """
-        if self.is_collection():
-            return self._impl.key
-        return self._column.name
-    name = property(name)
-
-    def is_collection(self):
-        """True iff this is a multi-valued (one-to-many or many-to-many) SA relation"""
-        return isinstance(self._impl, CollectionAttributeImpl)
-    
-    def collection_type(self):
-        """The type of object in the collection.  Calling this is only valid when `is_collection()` is True"""
-        return self._property.mapper.class_
-    
     def query(self, *args, **kwargs):
         """Perform a query in the parent's session"""
         return self.parent.session.query(*args, **kwargs)
     
-    def value(self):
-        """
-        The value of this attribute: use the corresponding value in the bound `data`,
-        if any; otherwise, use the value in the bound `model`.  If there is still no
-        value, use the default defined on the corresponding `Column`.
-        
-        For collections,
-        a list of the primary key values of the items in the collection is returned.
-        """
-        if self.parent.data is not None and self.name in self.parent.data:
-            v = unstr(self, self.parent.data[self.name])
-        else:
-            v = getattr(self.model, self.name)
-        if self.is_collection():
-            return [_pk(item) for item in v]
-        if v is not None:
-            return v
-        if self._column.default:
-            if callable(self._column.default.arg):
-                logger.info('Ignoring callable default value for %s' % self)
-            else:
-                return self._column.default
-        return None
-    value = property(value)
-    
-    def value_str(self):
-        """A string representation of `value` for use in non-editable contexts (so we don't check 'data')"""
-        if self.is_collection():
-            L = getattr(self.model, self.key)
-            return ','.join([str(item) for item in L])
-        else:
-            return str(getattr(self.model, self.key))
-        
-    def sync(self):
-        """Set the attribute's value in `model` to the value given in `data`"""
-        setattr(self.model, self.name, unstr(self, self.parent.data.get(self.name)))
-            
     def _validate(self):
         self.errors = []
 
         try:
-            value = unstr(self, self.parent.data.get(self.name))
+            value = self._unstr(self.parent.data.get(self.name))
         except validators.ValidationException, e:
             self.errors.append(e)
             return False
@@ -375,17 +259,29 @@ class AttributeRenderer(object):
         """True iff this attribute must be given a non-empty value"""
         return self._required
     
-    def __eq__(self, other):
-        try:
-            return self._impl is other._impl
-        except (AttributeError, ValueError):
-            return False
-    def __hash__(self):
-        return hash(self._impl)
-    
-    def __repr__(self):
-        return 'AttributeRenderer(%s)' % self._impl.key
-    
+    def _unstr(self, st, force_scalar=False):
+        """convert st (raw user data, or None) into the data type expected by attr"""
+        # todo handle non-object collections from AdditionalRenderer
+        if self.is_collection() and not force_scalar:
+            return [self.query(self.collection_type()).get(self._unstr(id_st, True))
+                    for id_st in st]
+        if isinstance(self.type, types.Boolean):
+            return st is not None
+        if st is None:
+            return None
+        if isinstance(self.type, types.Integer):
+            try:
+                return int(st)
+            except:
+                raise validators.ValidationException('Value is not an integer')
+        if isinstance(self.type, types.DateTime):
+            # todo
+            pass
+        if isinstance(self.type, types.Date):
+            # todo
+            pass
+        return st
+
     def model(self):
         return self.parent.model
     model = property(model)
@@ -395,7 +291,6 @@ class AttributeRenderer(object):
         attr = deepcopy(self)
         attr.parent = parent
         return attr
-    
     def validate(self, validator):
         attr = deepcopy(self)
         attr.validators.append(validator)
@@ -452,34 +347,8 @@ class AttributeRenderer(object):
             options = self.render_opts.get('options')
         attr.render_opts = {'multiple': multiple, 'options': options}
         return attr
-    def render(self, **html_options):
-        """
-        Render this attribute as HTML.
-        
-        `html_options` are not used by the default template, but are
-        provided to make more customization possible in custom templates
-        """
-        if not self.render_as:
-            self.render_as = self._get_render_as()
-        if isinstance(self._impl, ScalarObjectAttributeImpl) or self.is_collection():
-            if not self.render_opts.get('options'):
-                # todo this does not handle primaryjoin (/secondaryjoin) alternate join conditions
-                fk_cls = self.collection_type()
-                fk_pk = class_mapper(fk_cls).primary_key[0]
-                q = self.parent.session.query(fk_cls).order_by(fk_pk)
-                self.render_opts['options'] = query_options(q)
-                logger.debug('options for %s are %s' % (self.name, self.render_opts['options']))
-        if isinstance(self.type, types.Boolean) and not self.render_opts.get('options') and self.render_as in [SelectField, RadioSet]:
-            self.render_opts['options'] = [('True', True), ('False', False)]
-        opts = dict(self.render_opts)
-        opts.update(html_options)
-        return self.render_as(self, readonly=self.modifier=='readonly', disabled=self.modifier=='disabled', **opts).render()
+
     def _get_render_as(self):
-        if isinstance(self._impl, ScalarObjectAttributeImpl) or self.is_collection():
-            self.render_opts['multiple'] = self.is_collection()
-            if self.render_opts['multiple'] and 'size' not in self.render_opts:
-                self.render_opts['size'] = 5
-            return SelectField
         if isinstance(self.type, types.String):
             return TextField
         elif isinstance(self.type, types.Integer):
@@ -495,3 +364,200 @@ class AttributeRenderer(object):
         elif isinstance(self.type, types.Binary):
             return FileField
         return ModelField
+    
+    def render(self, **html_options):
+        """
+        Render this attribute as HTML.
+        
+        `html_options` are not used by the default template, but are
+        provided to make more customization possible in custom templates
+        """
+        opts = dict(self.render_opts)
+        opts.update(html_options)
+        return self.render_as(self, readonly=self.modifier=='readonly', disabled=self.modifier=='disabled', **opts).render()
+
+
+class AdditionalRenderer(AbstractRenderer):
+    """
+    Renderer for a manually-added form field
+    """
+    def __init__(self, parent, name, type, value):
+        AbstractRenderer.__init__(self, parent)
+        self.type = type
+        self.name = name
+        self.value = value
+        
+    def is_collection(self):
+        return self.render_opts.get('multiple', False)
+    
+    def value_str(self):
+        if self.is_collection():
+            return [str(item) for item in self.value]
+        return str(self.value)
+
+    def sync(self):
+        """Set the attribute's value in `model` to the value given in `data`"""
+        self.value = self._unstr(self.parent.data.get(self.name))
+            
+    def __repr__(self):
+        return 'AttributeRenderer(%s)' % self.name
+    
+    def render(self, **html_options):
+        if not self.render_as:
+            self.render_as = self._get_render_as()
+        return AbstractRenderer.render(self, **html_options)
+
+    def __eq__(self, other):
+        # we override eq so that when we configure with options=[...], we can match the renders in options
+        # with the ones that were generated at FieldSet creation time
+        try:
+            return self.name is other.name and self.model is other.model
+        except (AttributeError, ValueError):
+            return False
+    def __hash__(self):
+        return hash(self.name)
+
+
+class AttributeRenderer(AbstractRenderer):
+    """
+    Renderer for an SQLAlchemy attribute.
+    """
+    def __init__(self, instrumented_attribute, parent):
+        AbstractRenderer.__init__(self, parent)
+        # we rip out just the parts we care about from InstrumentedAttribute.
+        # impl is the AttributeImpl.  So far all we care about there is ".key,"
+        # which is the name of the attribute in the mapped class.
+        self._impl = instrumented_attribute.impl
+        # property is the PropertyLoader which handles all the interesting stuff.
+        # mapper, columns, and foreign keys are all located there.
+        self._property = instrumented_attribute.property
+        # smarter default "required" value
+        self._required = (not self.is_collection() and not self._column.nullable)
+            
+    def is_raw_foreign_key(self):
+        try:
+            return _foreign_keys(self._property.columns[0])
+        except AttributeError:
+            return False
+        
+    def is_pk(self):
+        return self._column.primary_key
+    
+    def type(self):
+        return self._column.type
+    type = property(type)
+
+    def _column(self):
+        # todo this does not allow handling composite attributes (PKs or FKs)
+        if isinstance(self._impl, ScalarObjectAttributeImpl):
+            # If the attribute is a foreign key, return the Column that this
+            # attribute is mapped from -- e.g., .user -> .user_id. 
+            return _foreign_keys(self._property)[0]
+        elif isinstance(self._impl, ScalarAttributeImpl):
+            # normal property, mapped to a single column from the main table
+            return self._property.columns[0]
+        else:
+            # collection -- use the mapped class's PK
+            assert isinstance(self._impl, CollectionAttributeImpl)
+            return self._property.mapper.primary_key[0]
+    _column = property(_column)
+    
+    def key(self):
+        """The name of the attribute in the class"""
+        return self._impl.key
+    key = property(key)
+
+    def name(self):
+        """ 
+        The name of the form input. usually the same as the column name, except for
+        multi-valued SA relation properties. For example, for order.user,
+        name will be user_id (assuming that is indeed the name of the foreign
+        key to users). 
+        """
+        if self.is_collection():
+            return self.key
+        return self._column.name
+    name = property(name)
+
+    def is_collection(self):
+        """True iff this is a multi-valued (one-to-many or many-to-many) SA relation"""
+        return isinstance(self._impl, CollectionAttributeImpl)
+    
+    def collection_type(self):
+        """The type of object in the collection (e.g., `User`).  Calling this is only valid when `is_collection()` is True"""
+        return self._property.mapper.class_
+    
+    def value(self):
+        """
+        The value of this attribute: use the corresponding value in the bound `data`,
+        if any; otherwise, use the value in the bound `model`.  If there is still no
+        value, use the default defined on the corresponding `Column`.
+        
+        For collections,
+        a list of the primary key values of the items in the collection is returned.
+        """
+        if self.parent.data is not None and self.name in self.parent.data:
+            v = self._unstr(self.parent.data[self.name])
+        else:
+            v = getattr(self.model, self.name)
+        if self.is_collection():
+            return [_pk(item) for item in v]
+        if v is not None:
+            return v
+        if self._column.default:
+            if callable(self._column.default.arg):
+                logger.info('Ignoring callable default value for %s' % self)
+            else:
+                return self._column.default
+        return None
+    value = property(value)
+    
+    def value_str(self):
+        """A string representation of `value` for use in non-editable contexts (so we don't check 'data')"""
+        if self.is_collection():
+            L = getattr(self.model, self.key)
+            return ','.join([str(item) for item in L])
+        else:
+            return str(getattr(self.model, self.key))
+        
+    def sync(self):
+        """Set the attribute's value in `model` to the value given in `data`"""
+        setattr(self.model, self.name, self._unstr(self.parent.data.get(self.name)))
+            
+    def __eq__(self, other):
+        # we override eq so that when we configure with options=[...], we can match the renders in options
+        # with the ones that were generated at FieldSet creation time
+        try:
+            return self._impl is other._impl and self.model is other.model
+        except (AttributeError, ValueError):
+            return False
+    def __hash__(self):
+        return hash(self._impl)
+    
+    def __repr__(self):
+        return 'AttributeRenderer(%s)' % self.key
+    
+    def render(self, **html_options):
+        if not self.render_as:
+            self.render_as = self._get_render_as()
+        if isinstance(self._impl, ScalarObjectAttributeImpl) or self.is_collection():
+            if not self.render_opts.get('options'):
+                # todo this does not handle primaryjoin (/secondaryjoin) alternate join conditions
+                fk_cls = self.collection_type()
+                fk_pk = class_mapper(fk_cls).primary_key[0]
+                q = self.parent.session.query(fk_cls).order_by(fk_pk)
+                self.render_opts['options'] = query_options(q)
+                logger.debug('options for %s are %s' % (self.name, self.render_opts['options']))
+        if isinstance(self.type, types.Boolean) and not self.render_opts.get('options') and self.render_as in [SelectField, RadioSet]:
+            self.render_opts['options'] = [('True', True), ('False', False)]
+        return AbstractRenderer.render(self, **html_options)
+
+    def _get_render_as(self):
+        if isinstance(self._impl, ScalarObjectAttributeImpl):
+            return SelectField
+        if self.is_collection():
+            self.render_opts['multiple'] = True
+            if 'size' not in self.render_opts:
+                self.render_opts['size'] = 5
+            return SelectField
+        return AbstractRenderer._get_render_as(self)
