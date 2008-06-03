372f6441a4cb9aba2f6b0e99ee885b588cf16ff6
jbellis <devnull@localhost>
2008-06-03 17:31:05 +0000

    release 0.3

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
 setup(name='FormAlchemy',
       license='MIT License',
-      version='0.2',
+      version='0.3',
       description='Auto-generated, customizable HTML input form fields from your SQLAlchemy mapped classes.',
       author='Alexandre Conrad',
       author_email='aconard(at@)go2france(dot.)com',
       url='http://formalchemy.googlecode.com',
       download_url='http://code.google.com/p/formalchemy/downloads/list',
-      packages=['formalchemy'],
-      install_requires=['mako']
+      packages=['formalchemy', 'formalchemy.tempita'],
       )
