fa0e4b19f7be2b721dd2c7f5f05d334394f5416d
jbellis <devnull@localhost>
2008-06-17 17:47:51 +0000

    add minlength, maxlength, regex, email validators

--- formalchemy/regression_validators.py
+++ formalchemy/regression_validators.py
@@ -36,6 +36,83 @@ ValidationException: Please specify full currency value, including cents (e.g., 
 Traceback (most recent call last):
 ...
 ValidationException: Please enter a value
+
+>>> minlength(0)('a')
+Traceback (most recent call last):
+...
+ValueError: Invalid minimum length
+>>> minlength(2)('a')
+Traceback (most recent call last):
+...
+ValidationException: Value must be at least 2 characters long
+>>> minlength(2)('ab')
+
+>>> maxlength(0)('a')
+Traceback (most recent call last):
+...
+ValueError: Invalid maximum length
+>>> maxlength(1)('a')
+>>> maxlength(1)('ab')
+Traceback (most recent call last):
+...
+ValidationException: Value must be no more than 1 characters long
+
+>>> regex('[A-Z]+$')('ASDF')
+>>> regex('[A-Z]+$')('abc')
+Traceback (most recent call last):
+...
+ValidationException: Invalid input
+>>> import re
+>>> pattern = re.compile('[A-Z]+$', re.I)
+>>> regex(pattern)('abc')
+
+>>> email('a+formalchemy@gmail.com')
+>>> email('a+."<>"@gmail.com')
+>>> email('a+."<>"."[]"@gmail.com')
+>>> email('a+."<>@gmail.com')
+Traceback (most recent call last):
+...
+ValidationException: Unterminated quoted section in recipient
+>>> email('a+."<>""[]"@gmail.com')
+Traceback (most recent call last):
+...
+ValidationException: Quoted section must be followed by '@' or '.'
+>>> email('<>@gmail.com')
+Traceback (most recent call last):
+...
+ValidationException: Reserved character present in recipient
+>>> email(chr(0) + '@gmail.com')
+Traceback (most recent call last):
+...
+ValidationException: Control characters present
+>>> email(chr(129) + '@gmail.com')
+Traceback (most recent call last):
+...
+ValidationException: Non-ASCII characters present
+>>> email('')
+Traceback (most recent call last):
+...
+ValidationException: Missing @ sign
+>>> email('@')
+Traceback (most recent call last):
+...
+ValidationException: Recipient must be non-empty
+>>> email('a@')
+Traceback (most recent call last):
+...
+ValidationException: Domain must be non-empty
+>>> email('a@gmail.com.')
+Traceback (most recent call last):
+...
+ValidationException: Domain must not end with '.'
+>>> email('a@gmail..com')
+Traceback (most recent call last):
+...
+ValidationException: Domain must not contain '..'
+>>> email('a@gmail>com')
+Traceback (most recent call last):
+...
+ValidationException: Reserved character present in domain
 """
 
 if __name__ == '__main__':
--- formalchemy/validators.py
+++ formalchemy/validators.py
@@ -1,3 +1,17 @@
+if 'any' not in locals():
+    # pre-2.5 support
+    def any(seq):
+        """
+        >>> any(xrange(10))
+        True
+        >>> any([0, 0, 0])
+        False
+        """
+        for o in seq:
+            if o:
+                return True
+        return False
+
 class ValidationException(Exception):
     def message(self):
         return self.args[0]
@@ -23,3 +37,92 @@ def float_(value):
 def currency(value):
     if '%.2f' % float_(value) != value:
         raise ValidationException('Please specify full currency value, including cents (e.g., 12.34)')
+
+def email(value):
+    reserved = r'()<>@,;:\"[]'
+
+    try:
+        recipient, domain = value.split('@', 1)
+    except ValueError:
+        raise ValidationException('Missing @ sign')
+
+    if any([ord(ch) < 32 for ch in value]):
+        raise ValidationException('Control characters present')
+    if any([ord(ch) > 127 for ch in value]):
+        raise ValidationException('Non-ASCII characters present')
+    
+    # validate recipient
+    if not recipient:
+        raise ValidationException('Recipient must be non-empty')
+    if recipient.endswith('.'):
+        raise ValidationException("Recipient must not end with '.'")
+
+    # quoted regions, aka the reason any regexp-based validator is wrong
+    i = 0
+    while i < len(recipient):
+        if recipient[i] == '"' and (i == 0 or recipient[i - 1] == '.' or recipient[i - 1] == '"'):
+            # begin quoted region -- reserved characters are allowed here.
+            # (this implementation allows a few addresses not strictly allowed by rfc 822 --
+            # for instance, a quoted region that ends with '\' appears to be illegal.)
+            i += 1
+            while i < len(recipient):
+                if recipient[i] == '"': 
+                    break # end of quoted region
+                i += 1
+            else: 
+                raise ValidationException("Unterminated quoted section in recipient")
+            i += 1
+            if i < len(recipient) and recipient[i] != '.': 
+                raise ValidationException("Quoted section must be followed by '@' or '.'")
+            continue
+        if recipient[i] in reserved: 
+            raise ValidationException("Reserved character present in recipient")
+        i += 1
+
+    # validate domain
+    if not domain:
+        raise ValidationException('Domain must be non-empty')
+    if domain.endswith('.'):
+        raise ValidationException("Domain must not end with '.'")
+    if '..' in domain:
+        raise ValidationException("Domain must not contain '..'")
+    if any([ch in reserved for ch in domain]):
+        raise ValidationException("Reserved character present in domain")
+
+
+# parameterized validators return the validation function
+def maxlength(length):
+    if length <= 0:
+        raise ValueError('Invalid maximum length')
+    def f(value):
+        if len(value) > length:
+            raise ValidationException('Value must be no more than %d characters long' % length)
+    return f
+
+def minlength(length):
+    if length <= 0:
+        raise ValueError('Invalid minimum length')
+    def f(value):
+        if len(value) < length:
+            raise ValidationException('Value must be at least %d characters long' % length)
+    return f
+
+def regex(exp, errormsg='Invalid input'):
+    import re
+    if type(exp) != type(re.compile('')):
+        exp = re.compile(exp)
+    def f(value):
+        if not exp.match(value):
+            raise ValidationException(errormsg)
+    return f
+
+# possible others:
+# oneof raises if input is not one of [or a subset of for multivalues] the given list of possibilities
+# url(check_exists=False)
+# address parts
+# cidr
+# creditcard number/securitycode (/expires?)
+# whole-form validators
+#   fieldsmatch
+#   requiredipresent/missing
+
