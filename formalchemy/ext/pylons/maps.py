42637c5a763cad53326713544a5c290a941a7ade
jbellis <devnull@localhost>
2008-10-22 12:41:42 +0000

    update docs

--- formalchemy/ext/pylons/maps.py
+++ formalchemy/ext/pylons/maps.py
@@ -6,6 +6,7 @@ def admin_map(map, cls, url):
     """connect the admin controller `cls` under the given `url`"""
     cname = cls.__name__.lower().split('controller')[0]
     log.info('connecting %s to %s' % (url, cname))
+    map.connect('/%s' % url, controller=cname, action='index')
     map.connect('/%s/' % url, controller=cname, action='index')
     map.connect('/%s/:modelname' % url, controller=cname, action='list')
     map.connect('/%s/:modelname/:action' % url, controller=cname)
