5a0d3e36c92118b504bcb04165f16fd305a9c355
jbellis <devnull@localhost>
2008-12-10 13:02:29 +0000

    finish i18n mv

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ args = dict(name='FormAlchemy',
       install_requires=['SQLAlchemy'],
       packages=['formalchemy', 'formalchemy.tempita',
                 'formalchemy.ext', 'formalchemy.ext.pylons'],
-      package_data={'formalchemy': ['i18n/*/LC_MESSAGES/formalchemy.mo',
+      package_data={'formalchemy': ['i18n_resources/*/LC_MESSAGES/formalchemy.mo',
                                     'ext/pylons/*.tmpl']},
       classifiers=[
           'Development Status :: 5 - Production/Stable',
