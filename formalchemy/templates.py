baf07418574a94d8702f402e047a61803937d33b
gael.pasgrimaud <devnull@localhost>
2009-02-02 19:34:34 +0000

    add options for admin controller

--- formalchemy/templates.py
+++ formalchemy/templates.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
-from paste.script.templates import Template
-from tempita import paste_script_template_renderer
 
-class PylonsTemplate(Template):
-    _template_dir = ('formalchemy', 'paster_templates/pylons_fa')
-    summary = 'Pylons template with formalchemy support'
-    required_templates = ['pylons']
-    template_renderer = staticmethod(paste_script_template_renderer)
-
+try:
+    from tempita import paste_script_template_renderer
+    from paste.script.templates import Template, var
+except ImportError:
+    class PylonsTemplate(object):
+        pass
+else:
+    class PylonsTemplate(Template):
+        _template_dir = ('formalchemy', 'paster_templates/pylons_fa')
+        summary = 'Pylons template with formalchemy support'
+        required_templates = ['pylons']
+        template_renderer = staticmethod(paste_script_template_renderer)
+        vars = [
+            var('admin_controller', 'Add formalchemy\'s admin controller',
+                default=False),
+            ]
