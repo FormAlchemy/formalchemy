eecd68fea8ef400a712ced08d430bef196246d21
gael.pasgrimaud <devnull@localhost>
2009-02-07 13:40:09 +0000

    it work

--- pylonsapp/pylonsapp/forms/__init__.py
+++ pylonsapp/pylonsapp/forms/__init__.py
@@ -1,7 +1,33 @@
-from formalchemy import forms
+from pylonsapp import model
+from pylonsapp.lib.base import render
 from formalchemy import validators
 from formalchemy import fields
-from pylonsapp.lib.base import render
+from formalchemy import forms
+from formalchemy import tables
 
 class FieldSet(forms.FieldSet):
-    pass
+    def _render(self, **kwargs):
+        return render('/fieldset.mako',
+                      extra_vars=kwargs)
+    def _render_readonly(self, **kwargs):
+        return render('/fieldset_readonly.mako',
+                      extra_vars=kwargs)
+
+class Grid(tables.Grid):
+    def _render(self, **kwargs):
+        return render('/grid.mako',
+                      extra_vars=kwargs)
+    def _render_readonly(self, **kwargs):
+        return render('/grid_readonly.mako',
+                      extra_vars=kwargs)
+
+## Initialize fieldsets
+
+#Foo = FieldSet(model.Foo)
+#Reflected = FieldSet(Reflected)
+
+## Initialize grids
+
+#FooGrid = Grid(model.Foo)
+#ReflectedGrid = Grid(Reflected)
+
