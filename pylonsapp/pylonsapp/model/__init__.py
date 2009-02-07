b304156557aabb01a5b44bac95925abbc4e34eb5
gael.pasgrimaud <devnull@localhost>
2009-02-07 12:54:44 +0000

    actualy broken but want to save my changes

--- pylonsapp/pylonsapp/model/__init__.py
+++ pylonsapp/pylonsapp/model/__init__.py
@@ -12,11 +12,30 @@ def init_model(engine):
     #                           autoload_with=engine)
     #orm.mapper(Reflected, reflected_table)
 
-    sm = orm.sessionmaker(autoflush=True, transactional=True, bind=engine)
+    sm = orm.sessionmaker(autoflush=True, autocommit=False, bind=engine)
 
     meta.engine = engine
     meta.Session = orm.scoped_session(sm)
 
+foo_table = sa.Table("Foo", meta.metadata,
+    sa.Column("id", sa.types.Integer, primary_key=True),
+    sa.Column("bar", sa.types.String(255), nullable=False),
+    )
+
+class Foo(object):
+    pass
+
+orm.mapper(Foo, foo_table)
+
+files_table = sa.Table("Files", meta.metadata,
+    sa.Column("id", sa.types.Integer, primary_key=True),
+    sa.Column("path", sa.types.String(255), nullable=False),
+    )
+
+class Files(object):
+    pass
+
+orm.mapper(Files, files_table)
 
 ## Non-reflected tables may be defined and mapped at module level
 #foo_table = sa.Table("Foo", meta.metadata,
