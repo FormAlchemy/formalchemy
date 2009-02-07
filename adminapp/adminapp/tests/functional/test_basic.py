7e87463b4e5806df78b97eec525b798b837f2efd
gael.pasgrimaud <devnull@localhost>
2009-02-07 14:21:30 +0000

    adminapp is not used anymore

--- adminapp/adminapp/tests/functional/test_basic.py
+++
@@ -1,7 +0,0 @@
-from adminapp.tests import *
-
-class TestBasicController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='basic', action='index'))
-        response.mustcontain('This is the bar field')
