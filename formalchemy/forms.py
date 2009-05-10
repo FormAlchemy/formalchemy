dc9674ed18e3040c087ac574cddcd340138cdf53
alexandre.conrad <devnull@localhost>
2009-05-10 12:27:16 +0000

    replaced aconrad's old email with new email

--- formalchemy/forms.py
+++ formalchemy/forms.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2007 Alexandre Conrad, aconrad(dot.)tlv(at@)magic(dot.)fr
+# Copyright (C) 2007 Alexandre Conrad, alexandre (dot) conrad (at) gmail (dot) com
 #
 # This module is part of FormAlchemy and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
