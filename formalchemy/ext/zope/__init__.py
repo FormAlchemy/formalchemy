36a2a572c1fa2f3f664a5fce4e2e6d095b985a4d
Gael Pasgrimaud <gael@gawel.org>
2009-08-22 13:26:37 +0200

    lot of improvement in ext.zope. just rock

--- formalchemy/ext/zope/__init__.py
+++ formalchemy/ext/zope/__init__.py
@@ -1,4 +1,87 @@
 # -*- coding: utf-8 -*-
+__doc__ = """This module provides an experimental subclass of
+:class:`~formalchemy.forms.FieldSet` to support zope.schema_'s schema.  `Simple
+validation`_ is supported. `Invariant`_ is not supported.
+
+.. _zope.schema: http://pypi.python.org/pypi/zope.schema
+.. _simple validation: http://pypi.python.org/pypi/zope.schema#simple-usage
+.. _invariant: http://pypi.python.org/pypi/zope.schema#schema-validation
+
+Available fields
+================
+
+Not all fields are supported. You can use TextLine, Text, Int, Bool, Float,
+Date, Datetime, Time.
+
+Usage
+=====
+
+Here is a simple example. First we need a schema::
+
+    >>> class IPet(interface.Interface):
+    ...     name = schema.Text(
+    ...         title=u'Name',
+    ...         required=True)
+    ...     type = schema.TextLine(
+    ...         title=u'Type',
+    ...         required=True)
+    ...     age = schema.Int(min=1)
+    ...     owner = schema.TextLine(
+    ...         title=u'Owner')
+    ...     birthdate = schema.Date(title=u'Birth date')
+
+Initialize FieldSet with schema::
+
+    >>> fs = FieldSet(IPet)
+
+Create a class to store values. If your class does not implement the form interface the FieldSet will generate an adapter for you::
+
+    >>> class Pet(FlexibleDict):pass
+    >>> p = Pet(name='dewey', type='cat', owner='gawel')
+    >>> fs = fs.bind(p)
+
+Fields are aware of schema attributes::
+
+    >>> fs.name.is_required()
+    True
+
+We can use the form::
+
+    >>> print fs.render().strip() #doctest: +ELLIPSIS
+    <div>
+      <label class="field_req" for="Pet--name">Name</label>
+      <textarea id="Pet--name" name="Pet--name">dewey</textarea>
+    </div>
+    ...
+    <div>
+      <label class="field_req" for="Pet--birthdate">Birth date</label>
+      <span id="Pet--birthdate"><select id="Pet--birthdate__month" name="Pet--birthdate__month"><option value="MM">Month</option>
+    <option value="1">January</option>
+    <option value="2">February</option>
+    ...
+
+Ok, let's assume that validation and syncing works::
+
+    >>> fs.configure(include=[fs.name])
+    >>> fs.rebind(p, data={'Pet--name':'minou'})
+    >>> fs.validate()
+    True
+    >>> fs.sync()
+    >>> fs.name.value
+    'minou'
+    >>> p.name
+    'minou'
+
+    >>> fs.configure(include=[fs.age])
+    >>> fs.rebind(p, data={'Pet--age':'-1'})
+    >>> fs.validate()
+    False
+    >>> fs.age.errors
+    [u'Value is too small']
+
+Look nice !
+
+"""
 from formalchemy.forms import FieldSet as BaseFieldSet
 from formalchemy.tables import Grid as BaseGrid
 from formalchemy.fields import Field as BaseField
@@ -24,8 +107,94 @@ FIELDS_MAPPING = {
         schema.Time: fatypes.Time,
     }
 
+class FlexibleModel(object):
+    """A flexible object to easy adapt most python classes:
+
+    .. sourcecode:: python
+
+        >>> obj = FlexibleModel(owner='gawel')
+        >>> obj.owner == obj.get('owner') == obj['owner'] == 'gawel'
+        True
+
+    """
+    def __init__(self, context=None, **kwargs):
+        if context is None:
+            self._context = dict(**kwargs)
+        else:
+            self._context = context
+
+    def __getattr__(self, attr):
+        """flexible __getattr__. also used for __getitem__ and get"""
+        if hasattr(self._context, attr):
+            return getattr(self._context, attr, None)
+        elif self._is_dict or isinstance(self._context, dict):
+            return self._context.get(attr)
+        raise AttributeError('%r as no attribute %s' % (self._context, attr))
+    __getitem__ = get = __getattr__
+
+    def __setattr__(self, attr, value):
+        """flexible __getattr__"""
+        if attr.startswith('_'):
+            return object.__setattr__(self, attr, value)
+        elif not self._is_dict and hasattr(self._context, attr):
+            setattr(self._context, attr, value)
+        elif self._is_dict or isinstance(self._context, dict):
+            self._context[attr] = value
+        else:
+            raise AttributeError('%r as no attribute %s' % (self._context, attr))
+    def __repr__(self):
+        return '<%s for %s>' % (self.__class__.__name__, repr(self._context))
+
+class FlexibleDict(FlexibleModel, dict):
+    """like FlexibleModel but inherit from dict:
+
+    .. sourcecode:: python
+
+        >>> obj = FlexibleModel(owner='gawel')
+        >>> obj.owner == obj.get('owner') == obj['owner'] == 'gawel'
+        True
+        >>> isinstance(obj, dict)
+        True
+
+    """
+    _is_dict = True
+
+_model_registry = {}
+
+def gen_model(iface, klass=None, dict_like=False):
+    """return a new FlexibleModel factory who provide iface:
+
+    .. sourcecode:: python
+
+        >>> class ITitle(interfaces.Interface):
+        ...     title = schema.TextLine(title=u'title')
+
+        >>> factory = gen_model(ITitle)
+        >>> obj = factory()
+        >>> ITitle.providedBy(obj)
+        True
+
+    """
+    if klass:
+        if not hasattr(klass, '__name__'):
+            klass = klass.__class__
+        name = class_name = klass.__name__
+    else:
+        class_name = None
+        name = iface.__name__[1:]
+    adapter = _model_registry.get((iface.__name__, class_name), None)
+    if adapter is not None:
+        return adapter
+    new_klass = type('%sAdapter' % name, (FlexibleModel,), dict(_is_dict=dict_like))
+    def adapter(context=None, **kwargs):
+        adapted = new_klass(context=context, **kwargs)
+        interface.directlyProvides(adapted, [iface])
+        return adapted
+    _model_registry[(iface.__name__, class_name)] = adapter
+    return adapter
+
 class Field(BaseField):
-    """"""
+    """Field aware of zope schema. See :class:`formalchemy.fields.AbstractField` for full api."""
     def value(self):
         if not self.is_readonly() and self.parent.data is not None:
             v = self._deserialize()
@@ -62,7 +231,6 @@ class Field(BaseField):
         try:
             bound.validate(value)
         except schema.ValidationError, e:
-            import pdb;pdb.set_trace()
             self.errors.append(e.doc())
 
         return not self.errors
@@ -74,6 +242,9 @@ class Field(BaseField):
 
 
 class FieldSet(BaseFieldSet):
+    """FieldSet aware of zope schema. See :class:`formalchemy.forms.FieldSet` for full api."""
+    auto_adapt = True
+    auto_gen = True
     def __init__(self, model, session=None, data=None, prefix=None):
         self._fields = OrderedDict()
         self._render_fields = OrderedDict()
@@ -94,21 +265,17 @@ class FieldSet(BaseFieldSet):
                 raise NotImplementedError('%s is not mapped to a type' % klass)
             else:
                 self.append(Field(name=name, type=t))
-                if field.title:
-                    self._fields[name].label_text = field.title
+                self._fields[name].label_text = field.title or name
+                if field.description:
+                    self._fields[name].set(instructions=field.description)
                 if field.required:
                     self._fields[name].validators.append(validators.required)
                 if klass is schema.Text:
                     self._fields[name].set(renderer=fields.TextAreaFieldRenderer)
 
     def bind(self, model, session=None, data=None):
-        """Bind to an instance"""
-        if not (model or session or data):
+        if not (model is not None or session or data):
             raise Exception('must specify at least one of {model, session, data}')
-        if not model:
-            if not self.model:
-                raise Exception('model must be specified when none is already set')
-            model = fields._pk(self.model) is None and type(self.model) or self.model
         # copy.copy causes a stacktrace on python 2.5.2/OSX + pylons.  unable to reproduce w/ simpler sample.
         mr = object.__new__(self.__class__)
         mr.__dict__ = dict(self.__dict__)
@@ -120,12 +287,17 @@ class FieldSet(BaseFieldSet):
                                              [field.bind(mr) for field in self._render_fields.itervalues()]])
         return mr
 
+    def gen_model(self, model=None):
+        factory = gen_model(self.iface, model)
+        model = factory(model)
+        return model
+
     def rebind(self, model, session=None, data=None):
-        if model:
-            if not self.iface.providedBy(model):
-                raise Exception('%r is not provided by %r' % (self.iface, model))
-            self.model = model
-            self._bound_pk = None
+        if model is not self.iface:
+            if model and not self.iface.providedBy(model):
+                model = self.gen_model(model)
+        self.model = model
+        self._bound_pk = None
         if data is None:
             self.data = None
         elif hasattr(data, 'getall') and hasattr(data, 'getone'):
@@ -137,6 +309,7 @@ class FieldSet(BaseFieldSet):
                 raise Exception('unsupported data object %s.  currently only dicts and Paste multidicts are supported' % self.data)
 
 class Grid(BaseGrid, FieldSet):
+    """Grid aware of zope schema. See :class:`formalchemy.tables.Grid` for full api."""
     def __init__(self, cls, instances=[], session=None, data=None, prefix=None):
         FieldSet.__init__(self, cls, session, data, prefix)
         self.rows = instances
@@ -159,54 +332,4 @@ class Grid(BaseGrid, FieldSet):
     def _set_active(self, instance, session=None):
         FieldSet.rebind(self, instance, session or self.session, self.data)
 
-def test_fieldset():
-
-    class IPet(interface.Interface):
-        name = schema.Text(
-            title=u'Name',
-            required=True)
-        type = schema.TextLine(
-            title=u'Type',
-            required=True)
-        age = schema.Int(min=1)
-        owner = schema.TextLine(
-            title=u'Owner')
-        birthdate = schema.Date(title=u'Birth date')
-
-    class Pet(object):
-        interface.implements(IPet)
-        def __init__(self):
-            self.name = ''
-            self.type = ''
-            self.age = ''
-            self.owner = ''
-            self.birthdate = ''
-
-    fs = FieldSet(IPet)
-    p = Pet()
-    p.name = 'dewey'
-    p.type = 'cat'
-    p.owner = 'gawel'
-    fs = fs.bind(p)
-
-    # rendering
-    assert fs.name.is_required() == True, fs.name.is_required()
-    assert fs.owner.value == 'gawel', fs.owner.value
-    html = fs.render()
-    assert 'class="field_req" for="Pet--name"' in html, html
-    assert 'value="gawel"' in html, html
-    assert 'Birth date' in html, html
-
-    # syncing
-    fs.configure(include=[fs.name])
-    fs.rebind(p, data={'Pet--name':'minou'})
-    fs.validate()
-    fs.sync()
-    assert fs.name.value == 'minou', fs.render_fields
-    assert p.name == 'minou', p.name
-
-    fs.configure(include=[fs.age])
-    fs.rebind(p, data={'Pet--age':'-1'})
-    fs.validate()
-    assert fs.age.errors == [u'Value is too small'], fs.errors
 
