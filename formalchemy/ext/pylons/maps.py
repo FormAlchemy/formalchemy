1b1f03dcf13770b22139b856208691f8e41d368a
Gael Pasgrimaud <gael@gawel.org>
2009-09-26 22:21:53 +0200

    need space

--- formalchemy/ext/pylons/maps.py
+++ formalchemy/ext/pylons/maps.py
@@ -23,38 +23,50 @@ def admin_map(map, controller, url='%s'):
 
     map.connect('admin', '%s' % url,
         controller=controller, action='index')
+
     map.connect('formatted_admin', '%s.{format}' % url,
         controller=controller, action='index')
+
     map.connect("models", "%s/{modelname}" % url,
         controller=controller, action="edit", id=None, format='html',
         conditions=dict(method=["POST"], function=format))
+
     map.connect("models", "%s/{modelname}" % url,
         controller=controller, action="list",
         conditions=dict(method=["GET"], function=format))
+
     map.connect("formatted_models", "%s/{modelname}.{format}" % url,
         controller=controller, action="list",
         conditions=dict(method=["GET"]))
+
     map.connect("new_model", "%s/{modelname}/new" % url,
         controller=controller, action="edit", id=None,
         conditions=dict(method=["GET"]))
+
     map.connect("formatted_new_model", "%s/{modelname}/new.{format}" % url,
         controller=controller, action="edit", id=None,
         conditions=dict(method=["GET"]))
+
     map.connect("%s/{modelname}/{id}" % url,
         controller=controller, action="edit",
         conditions=dict(method=["PUT"], function=format))
+
     map.connect("%s/{modelname}/{id}" % url,
         controller=controller, action="delete",
         conditions=dict(method=["DELETE"]))
+
     map.connect("edit_model", "%s/{modelname}/{id}/edit" % url,
         controller=controller, action="edit",
         conditions=dict(method=["GET"]))
+
     map.connect("formatted_edit_model", "%s/{modelname}/{id}.{format}/edit" % url,
         controller=controller, action="edit",
         conditions=dict(method=["GET"]))
+
     map.connect("view_model", "%s/{modelname}/{id}" % url,
         controller=controller, action="edit",
         conditions=dict(method=["GET"], function=format))
+
     map.connect("formatted_view_model", "%s/{modelname}/{id}.{format}" % url,
         controller=controller, action="edit",
         conditions=dict(method=["GET"]))
