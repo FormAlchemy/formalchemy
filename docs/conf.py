5ea75722bf83397e40539bf28b73a921cb09cfee
Gael Pasgrimaud <gael@gawel.org>
2010-04-07 17:02:13 +0200

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ copyright = '2008, Alexandre Conrad'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.3.3'
+version = '1.3.4'
 # The full version, including alpha/beta/rc tags.
 release = version
 
