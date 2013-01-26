00952b37bb798a510e519b66153407c5bb457730
Gael Pasgrimaud <gael@gawel.org>
2013-01-26 16:05:15 +0100

    back to dev

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ long_description = '.. contents::\n\n' +\
                    '=======\n\n' +\
                    read('CHANGELOG.txt')
 
-version='1.4.3'
+version='1.4.4-dev'
 
 setup(name='FormAlchemy',
       license='MIT License',
