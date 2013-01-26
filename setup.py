1f34e86cd75075732259473086c50de1eb62ebfa
Gael Pasgrimaud <gael@gawel.org>
2013-01-26 16:03:11 +0100

    bump version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ long_description = '.. contents::\n\n' +\
                    '=======\n\n' +\
                    read('CHANGELOG.txt')
 
-version='1.4.3dev'
+version='1.4.3'
 
 setup(name='FormAlchemy',
       license='MIT License',
