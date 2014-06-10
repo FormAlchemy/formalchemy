3aa2aa7ad0eb121136e5101a003e5943a94d30df
Matthias Urlichs <matthias@urlichs.de>
2014-06-10 18:37:59 +0200

    exc detail

--- formalchemy/tests/test_validate.py
+++ formalchemy/tests/test_validate.py
@@ -4,7 +4,7 @@ from formalchemy.tests import *
 def validate_empty():
     """
     >>> fs = FieldSet(bill)
-    >>> fs.validate()
+    >>> fs.validate() #doctest: +ELLIPSIS,+IGNORE_EXCEPTION_DETAIL
     Traceback (most recent call last):
     ...
     ValidationError: Cannot validate without binding data
