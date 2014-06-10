0669f8a70c0d5dfb2601b0a4526f483d8b48a9b7
Matthias Urlichs <matthias@urlichs.de>
2014-06-10 19:09:19 +0200

    Update version

--- formalchemy/__init__.py
+++ formalchemy/__init__.py
@@ -46,5 +46,5 @@ def Column(*args, **kwargs):
 
 
 __all__ = ["FieldSet", "Field", "FieldRenderer", "Grid", "ValidationError", "validators", "SimpleMultiDict", "types"]
-__version__ = "1.5.0-dev"
+__version__ = "1.5.0"
 
